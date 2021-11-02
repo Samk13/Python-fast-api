3a524d3cf651522dc341e98685aebea388e8b584
Samk13 <samarbid13@gmail.com>
2021-11-02 23:56:13 +0100

    make  basic post reqiuest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -14,3 +15,9 @@ def get_posts():
         {"id": 1, "title": "Hello World"},
         {"id": 2, "title": "Hello Galaxy"},
     ]
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"the new post is {payload['id']} with title {payload['title']}"}

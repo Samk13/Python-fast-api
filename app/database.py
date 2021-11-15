35f288cb9e56b3b6be3613187283f583e8c4b5f8
Samk13 <samarbid13@gmail.com>
2021-11-15 22:37:34 +0100

    gard from random errors with sqlalchemy

--- app/database.py
+++ app/database.py
@@ -5,7 +5,16 @@ from sqlalchemy.ext.declarative import declarative_base
 # TODO move this line to the secrets
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/postgres"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    },
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 

9a0fcde866b6a03c1647678cf840de3160353a45
Samk13 <samarbid13@gmail.com>
2021-11-17 23:50:08 +0100

    implement get user by id

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+from .utils import hash_password
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor
 from . import models, schemas
@@ -148,8 +149,21 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User Email already registered",
         )
+    # Hash password with bcrypt
+    hashed_password = hash_password(user.password)
+    user.password = hashed_password
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get("/users/{user_id}", response_model=schemas.UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"User id {user_id} not found"
+        )
+    return user

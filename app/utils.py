1bda48f32a113ccfcc2669d7894a21b03c8ea2df
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2021-11-18 00:38:47 +0100

    refactor and create router (#1)

--- app/utils.py
+++ app/utils.py
@@ -5,3 +5,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password):
     return pwd_context.hash(password)
+
+
+def find_post(id):
+    for post in my_posts:
+        if int(post["id"]) == int(id):
+            return post
+    return None

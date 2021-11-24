81c7a056d73bce9253ecc4b1b85e5bc55a5746e9
Samk13 <samarbid13@gmail.com>
2021-11-24 23:34:59 +0100

    reformat using black

--- alembic/versions/265d61ecbf99_add_foreign_key_to_posts_table.py
+++ alembic/versions/265d61ecbf99_add_foreign_key_to_posts_table.py
@@ -10,33 +10,26 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '265d61ecbf99'
-down_revision = '360bda0494c8'
+revision = "265d61ecbf99"
+down_revision = "360bda0494c8"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column(
-        'posts',
-        sa.Column(
-            'owner_id',
-            sa.Integer,
-            nullable=False
-        )
-    )
+    op.add_column("posts", sa.Column("owner_id", sa.Integer, nullable=False))
     op.create_foreign_key(
-        'posts_users_fk',
-        source_table='posts',
-        referent_table='users',
-        local_cols=['owner_id'],
-        remote_cols=['id'],
-        ondelete='CASCADE'
+        "posts_users_fk",
+        source_table="posts",
+        referent_table="users",
+        local_cols=["owner_id"],
+        remote_cols=["id"],
+        ondelete="CASCADE",
     )
     pass
 
 
 def downgrade():
-    op.drop_constraint('posts_users_fk', table_name='posts')
-    op.drop_column('posts', 'owner_id')
+    op.drop_constraint("posts_users_fk", table_name="posts")
+    op.drop_column("posts", "owner_id")
     pass
--- alembic/versions/a9333f8e681c_add_the_rest_of_columns_to_posts_table.py
+++ alembic/versions/a9333f8e681c_add_the_rest_of_columns_to_posts_table.py
@@ -10,36 +10,36 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a9333f8e681c'
-down_revision = '265d61ecbf99'
+revision = "a9333f8e681c"
+down_revision = "265d61ecbf99"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.add_column(
-        'posts',
+        "posts",
         sa.Column(
-            'published',
+            "published",
             sa.Boolean,
             nullable=False,
             server_default=sa.text("True"),
-        )
+        ),
     )
     op.add_column(
-        'posts',
+        "posts",
         sa.Column(
-            'created_at',
+            "created_at",
             sa.TIMESTAMP(timezone=True),
             nullable=False,
             server_default=sa.text("now()"),
-        )
+        ),
     )
     pass
 
 
 def downgrade():
-    op.drop_column('posts', 'published')
-    op.drop_column('posts', 'created_at')
+    op.drop_column("posts", "published")
+    op.drop_column("posts", "created_at")
 
     pass

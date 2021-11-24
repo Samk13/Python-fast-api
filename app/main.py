2b845fc1e3eaf79fff222ee93b2a2c9dbeaf71cd
Samk13 <samarbid13@gmail.com>
2021-11-24 23:29:42 +0100

    setup CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 from .database import engine
 from . import models
@@ -8,6 +9,17 @@ app = FastAPI()
 
 # No need for this anymore since we are using Alembic
 # models.Base.metadata.create_all(bind=engine)
+
+# add CORS
+# add accepted origins
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # add routers
 app.include_router(post.router)

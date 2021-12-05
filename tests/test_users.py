306e8d305f0e7266a4c8d90459ffd558a5409a0a
Samk13 <samarbid13@gmail.com>
2021-12-05 22:45:48 +0100

    add test create user

--- tests/test_users.py
+++ tests/test_users.py
@@ -8,3 +8,9 @@ def test_root():
     res = client.get("/")
     assert res.status_code == 200
     assert res.json().get('message') == "Hello World during the coronavirus pandemic! 🎉"
+
+
+def test_create_user():
+    res = client.post(
+        "/users/", json={"email": "test1@test.se", "password": "test_password"})
+    assert res.status_code == 201

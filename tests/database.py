e03be91cffb2e5e1a821a0118d6bf85b521b0350
Samk13 <samarbid13@gmail.com>
2021-12-07 00:45:10 +0100

    refactor test db

---
+++ tests/database.py
@@ -0,0 +1,40 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.main import app
+
+from app.config import settings
+from app.database import get_db
+from app.database import Base
+
+# Setup  test db for test
+# you should create new database for test in tableplus or your db manager
+
+
+@pytest.fixture()
+def session():
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    TestingSessionLocal = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine)
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,57 +1,6 @@
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.main import app
-from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db
-from app.database import Base
-import pytest
-# Setup  test db for test
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True,
-    connect_args={
-        "keepalives": 1,
-        "keepalives_idle": 30,
-        "keepalives_interval": 10,
-        "keepalives_count": 5,
-    },
-)
-testingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
-
-# Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    db = testingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-# Swapping out the get_db function with the testing one
-app.dependency_overrides[get_db] = override_get_db
-
-# End setup test db for test
-
-# Setup fixture for test
-
-
-@pytest.fixture
-def client():
-    # drop tables
-    Base.metadata.drop_all(bind=engine)
-    # create tables
-    Base.metadata.create_all(bind=engine)
-# Change return to yield so we can run code before and after the testClient
-    yield TestClient(app)
+from .database import client, session
+#  should import session even though it's not called directly cuase client is dependent on it
 
 
 def test_root(client):

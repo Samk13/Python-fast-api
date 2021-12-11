e359f44268ca51e629c1082c787f712ce7112723
Samk13 <samarbid13@gmail.com>
2021-12-12 00:54:55 +0100

    test login user

--- app/calc.py
+++ app/calc.py
@@ -6,7 +6,7 @@ class InsufficientFunds(Exception):
     pass
 
 
-class BankAccount():
+class BankAccount:
     def __init__(self, starting_balance=0):
         self.balance = starting_balance
 
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,7 @@
 from app import schemas
-from .database import client, session
+from jose import jwt
+from app.config import settings
+
 #  should import session even though it's not called directly cuase client is dependent on it
 
 
@@ -16,3 +18,21 @@ def test_create_user(client):
     new_user = schemas.UserResponse(**res.json())
     assert res.status_code == 201
     assert res.json().get("email") == "test@test.se"
+
+
+def test_login_user(client, test_create_login_user):
+    res = client.post(
+        "/login",
+        data={
+            "username": test_create_login_user["email"],
+            "password": test_create_login_user["password"],
+        },
+    )
+    login_res = schemas.TokenResponse(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
+    id = payload.get("user_id")
+    assert id == test_create_login_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200

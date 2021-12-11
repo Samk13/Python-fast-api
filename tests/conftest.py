663609c7ce59b2087949eddd5a22865d05ec3add
Samk13 <samarbid13@gmail.com>
2021-12-12 00:54:27 +0100

    refactor tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+# You can define fixtures here all tests will have access to them automatically
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
@@ -13,8 +14,10 @@ from app.database import Base
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-TestingSessionLocal = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# add scope="module" so the fixture is only executed once in the module
+# https://youtu.be/0sOvCWFmrtA?t=57683
 
 
 @pytest.fixture()
@@ -36,5 +39,19 @@ def client(session):
             yield session
         finally:
             session.close()
+
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
+
+
+# create fixture for cerating user
+
+
+@pytest.fixture
+def test_create_login_user(client):
+    user_data = {"email": "test@test.se", "password": "test_password"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user

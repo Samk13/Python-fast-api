c23ed3aed5fa1f0eb1a2499bb1e7033716d06d75
Samk13 <samarbid13@gmail.com>
2021-12-12 16:58:07 +0100

    parametrize user input test

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -18,11 +18,11 @@ def login(
     )
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect email or password"
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
         )
     if not utils.verify_password(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect email or password"
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
         )
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 from app import schemas
 from jose import jwt
 from app.config import settings
@@ -36,3 +37,32 @@ def test_login_user(client, test_create_login_user):
     assert id == test_create_login_user["id"]
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrong_email@mail.se", "password", 403),
+        ("test@test.se", "wrong_password", 403),
+        ("wrong_email@mail.se", "wrong_password", 403),
+        (None, "password", 422),
+        ("test@test.se", None, 422),
+    ],
+)
+def test_incorrect_login_user(client, email, password, status_code):
+    res = client.post(
+        "/login",
+        data={
+            "username": email,
+            "password": password,
+        },
+    )
+    assert res.status_code == status_code
+    if email is None:
+        assert dict(res.json()).get("detail")[0].get("type") == "value_error.missing"
+        assert dict(res.json()).get("detail")[0].get("msg") == "field required"
+    if password is None:
+        assert dict(res.json()).get("detail")[0].get("type") == "value_error.missing"
+        assert dict(res.json()).get("detail")[0].get("msg") == "field required"
+    if email is not None and password is not None:
+        assert res.json().get("detail") == "Invalid Credentials"

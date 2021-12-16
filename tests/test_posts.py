a1628b844e2def57ecb72aa3d78651d4fd108e64
Samk13 <samarbid13@gmail.com>
2021-12-16 22:11:50 +0100

    test retreive posts not exists

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -26,3 +26,8 @@ def test_unauthorized_get_one_post(client, test_posts):
     print(vars(test_posts[0]))
     res = client.get(f"/posts/{test_posts[0].id}")
     assert res.status_code == 401
+
+
+def test_get_one_post_not_exists(authorized_client, test_posts):
+    res = authorized_client.get('/posts/100')
+    assert res.status_code == 404
